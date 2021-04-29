c29a39bb3518b3edc2afe88eda810dbf3a93c7ff
kbrzdem <kbr_ozdm@hotmail.com>
2021-04-29 23:18:26 +0300

    Semantic HTML kullanılarak yapı oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from  "./components/Header";
+import Dashboard from  "./components/Dashboard";
+export const headerLinks = [
+  {
+    "id": 1,
+    "title": "Dashboard",
+    "link": "/",
+  },
+  {
+    "id": 2,
+    "title": "Profile",
+    "link": "/profile"
+  },
+  {
+    "id": 3,
+    "title": "Utilities",
+    "link": "/utilities"
+  },
+  {
+    "id": 4,
+    "title": "Settings",
+    "link": "/settings"
+  },
+  {
+    "id": 4,
+    "title": "Messages",
+    "link": "/messages"
+  },
+  {
+    "id": 4,
+    "title": "Analytics",
+    "link": "/analytics"
+  },
+  {
+    "id": 4,
+    "title": "Integrations",
+    "link": "/ıntegrations"
+  }
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Header headers={headerLinks}/>
+      <main>
+        <Switch>
+            <Route>
+              <Dashboard />
+            </Route>
+          </Switch>
+      </main>
+        
+    </Router>
   );
 }
 

f452a44bd969c21de9bd5ece00adfa2b8cd1b4e1
kbrzdem <kbr_ozdm@hotmail.com>
2021-04-30 23:08:00 +0300

    graphic comp oluşturuldu üst kısım jsx yazıldı

---
+++ src/components/Graphic.js
@@ -0,0 +1,27 @@
+
+
+
+const Graphic = () => {
+    return (
+        <>
+            <div className="profile">
+                <div className="profile__growth">
+                    <div className="profile__overal">
+                        <h1>Profile Growth</h1>
+                        <p>Overal Information</p>
+                    </div>
+                    <div className="button">
+                        <a className="button__one">Monts</a>
+                        <a className="button__two">Years</a>
+                    </div>
+                </div>
+                <div className="profile__graphic">
+
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+export default Graphic;

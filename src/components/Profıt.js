dfdcd534a43fcf884acfa743e718ecc7f77fad31
kbrzdem <kbr_ozdm@hotmail.com>
2021-04-30 23:07:03 +0300

    comp oluşturuldu jsx yapısı düzenlendi

---
+++ src/components/Profıt.js
@@ -0,0 +1,35 @@
+
+
+
+const Profıt = () => {
+    return (
+        <>
+            <div className="profıtcard">
+                <p className="profıtcard__title">Profıt list</p>
+                <p className="profıtcard__title" >David</p>
+                <p className="profıtcard__title">CEO</p>
+                <p className="profıtcard__title">02-02-2021</p>
+                <p className="profıtcard__desc5 profıtcard__desc5--background">Pending</p>
+
+            </div> 
+            <div className="profıtcard">
+                <p className="profıtcard__title" >Growth Ratio</p>
+                <p className="profıtcard__title" >Mike</p>
+                <p className="profıtcard__title">CTO</p>
+                <p className="profıtcard__title">02-03-2021</p>
+                <p className="profıtcard__desc5 profıtcard__desc5--background">Done</p>
+
+            </div> 
+            <div className="profıtcard">
+                <p className="profıtcard__title">Earnigs</p>
+                <p className="profıtcard__title" >Ghulam</p>
+                <p className="profıtcard__title">Manager</p>
+                <p className="profıtcard__title">02-04-2021</p>
+                <p className="profıtcard__desc5 profıtcard__desc5--background">Testing</p>
+            </div>
+           
+        </>
+    )
+};
+
+export default Profıt;

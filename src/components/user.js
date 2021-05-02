04d96ddc1c7e0d3260d8632ddc77fef3b3425c03
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-02 16:47:38 +0300

    Open, Analytics ve Audit comp oluşturuldu

--- src/components/user.js
+++ src/components/user.js
@@ -1,9 +1,19 @@
-
+import Open from  "./Open";
+import Analytics from  "./Analytics";
+import Audit from  "./Audit";
 
 const User = () => {
     return (
         <>
-            
+            <div className="open" >
+                <Open />
+            </div>
+            <div className="analytics" >
+                <Analytics />
+            </div>
+            <div className="audit" >
+                <Audit />
+            </div>
         </>
     )
 };

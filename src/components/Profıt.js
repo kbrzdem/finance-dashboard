f5c635a97e38b034ac5984be92d4c759e07a4f5e
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-06 03:02:42 +0300

    responsive düzenleme

--- src/components/Profıt.js
+++ src/components/Profıt.js
@@ -1,56 +1,56 @@
-
-
-
 const Profıt = () => {
-    return (
-        <>
-            <div class="col-6 col-md-6 col-lg-12">
-            <div className="profıtcard">
-                <p className="profıtcard__title">Profıt list</p>
-                <p className="profıtcard__title" >David</p>
-                <p className="profıtcard__title">CEO</p>
-                <p className="profıtcard__title">02-02-2021</p>
-                <p className="profıtcard__desc5 profıtcard__desc5--background">Pending</p>
-                <div className="profıtcard__after">
-                <span className="after after--profıt" ></span>
-                <span className="after after--card after--profıt after--profıt--one" ></span>
-                <span className="after after--card2 after--profıt after--profıt--two" ></span>
-
-                </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 col-lg-12">
-            <div className="profıtcard">
-                <p className="profıtcard__title" >Growth Ratio</p>
-                <p className="profıtcard__title" >Mike</p>
-                <p className="profıtcard__title">CTO</p>
-                <p className="profıtcard__title">02-03-2021</p>
-                <p className="profıtcard__desc5 profıtcard__desc5--background">Done</p>
-                <div className="profıtcard__after">
-                <span className="after after--profıt" ></span>
-                <span className="after after--card after--profıt after--profıt--one" ></span>
-                <span className="after after--card2 after--profıt after--profıt--two" ></span>
-
-                </div>
-                </div>   
-            </div>
-            <div class="col-6 col-md-6 col-lg-12">
-            <div className="profıtcard">
-                <p className="profıtcard__title">Earnigs</p>
-                <p className="profıtcard__title" >Ghulam</p>
-                <p className="profıtcard__title">Manager</p>
-                <p className="profıtcard__title">02-04-2021</p>
-                <p className="profıtcard__desc5 profıtcard__desc5--background">Testing</p>
-                <div className="profıtcard__after">
-                <span className="after after--profıt" ></span>
-                <span className="after after--card after--profıt after--profıt--one" ></span>
-                <span className="after after--card2 after--profıt after--profıt--two" ></span>
-                </div>
-                </div>
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <div class="col-6 col-md-6 col-lg-12">
+        <div className="profıtcard">
+          <p className="profıtcard__title">Profıt list</p>
+          <p className="profıtcard__title">David</p>
+          <p className="profıtcard__title">CEO</p>
+          <p className="profıtcard__title">02-02-2021</p>
+          <p className="profıtcard__desc5 profıtcard__desc5--background">
+            Pending
+          </p>
+          <div className="profıtcard__after">
+            <span className="after after--profıtcard"></span>
+            <span className="after after--card after--profıtcard after--profıtcard--one"></span>
+            <span className="after after--card2 after--profıtcard after--profıtcard--two"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-6 col-lg-12">
+        <div className="profıtcard">
+          <p className="profıtcard__title">Growth Ratio</p>
+          <p className="profıtcard__title">Mike</p>
+          <p className="profıtcard__title">CTO</p>
+          <p className="profıtcard__title">02-03-2021</p>
+          <p className="profıtcard__desc5 profıtcard__desc5--background">
+            Done
+          </p>
+          <div className="profıtcard__after">
+            <span className="after after--profıtcard"></span>
+            <span className="after after--card after--profıtcard after--profıtcard--one"></span>
+            <span className="after after--card2 after--profıtcard after--profıtcard--two"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-6 col-lg-12">
+        <div className="profıtcard">
+          <p className="profıtcard__title">Earnigs</p>
+          <p className="profıtcard__title">Ghulam</p>
+          <p className="profıtcard__title">Manager</p>
+          <p className="profıtcard__title">02-04-2021</p>
+          <p className="profıtcard__desc5 profıtcard__desc5--background">
+            Testing
+          </p>
+          <div className="profıtcard__after">
+            <span className="after after--profıtcard"></span>
+            <span className="after after--card after--profıtcard after--profıtcard--one"></span>
+            <span className="after after--card2 after--profıtcard after--profıtcard--two"></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profıt;

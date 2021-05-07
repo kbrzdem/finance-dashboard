9b6b7c72a832065be1c7667b16fb77ed68fc2131
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-07 07:04:29 +0300

    bar grafiği yıllar eklendi comp olşturuldu

---
+++ src/components/Chartyears.js
@@ -0,0 +1,12 @@
+const  chartyears = () => {
+    return(
+            <div className="profile__chartyears">
+                <p className=" profile__chartyears--text">2021</p>
+                <p className=" profile__chartyears--text">2020</p>
+                <p className=" profile__chartyears--text">2019</p>
+                <p className=" profile__chartyears--text">2018</p>  
+            </div>
+    )
+};
+
+export default chartyears

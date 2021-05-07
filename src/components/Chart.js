92fe3ae79b76e681fbabbe2c41767bd4ff1f4a90
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-07 07:05:34 +0300

    eksikler tamamlandı

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,12 +1,14 @@
 import data from "./data.json";
+import Chartmonths from "./chartmonths";
+import Chartyears from "./Chartyears";
 
+//vievBox={"0 0 ${400px} ${height}"} 
 const Chart = ({ children, height, width }) => (
-  <svg vievBox={"0 0 ${400px} ${height}"} height={height} width={400}>
+  <svg height={height} width={400}>
     {children}
+    
   </svg>
 );
-
-
 
 const Bar = ({
   fill = "#8D49F2",
@@ -25,19 +27,18 @@ const greatestValue = (values) =>
   values.reduce((acc, cur) => (cur > acc ? cur : acc), -Infinity);
 
 const BarChart = ({ data }) => {
-  const barWidth = 15;
-  const barMargin = 35;
-  const rx = 10;
+  const barWidth = 16;
+  const barMargin = 37;
   const width = data.length * (barWidth + barMargin);
   const height = greatestValue(data.map((datam) => datam.LessonsWatched));
   return (
-    <Chart height={height} width={width}>
+    <Chart height={height} width={width} >
       {data.map((datam, index) => (
         <Bar
-          rx="7"
-          ry="7"
+          rx="8"
+          ry="8"
           key={datam.name}
-          fill="#8D49F2"
+          fill="#5e27c5ec"
           x={index * (barWidth + barMargin)}
           y={height - datam.LessonsWatched}
           width={barWidth}
@@ -53,7 +54,6 @@ const Bchart = () => {
     <>
       <div className="profile">
         <div className="profile__growth">
-
           <div className="col-6 col-md-6 col-lg-7">
             <div className="profile__overal">
               <h3>Profile Growth</h3>
@@ -74,7 +74,30 @@ const Bchart = () => {
         </div>
         
         <div className="profile__chart">
+        
           <BarChart data={data} />
+          
+          <div className="profile__chartsept">
+            <div>
+            <span className="profile__chart--sept">10 of September</span>
+
+            </div>
+            <div>
+            <p className="profile__chart--dlr">
+              $123001
+            </p>
+            </div>
+          </div> 
+          
+          <span className="profile__chart--number">90</span>
+          <span className="profile__chart--number profile__chart--number1">130</span>
+          <span className="profile__chart--number profile__chart--number2">190</span>
+          <span className="profile__chart--border"></span>
+          <span className="profile__chart--border profile__chart--border--1"></span>
+          <span className="profile__chart--border profile__chart--border--2"></span>
+          <span className="profile__chart--border profile__chart--border--3"></span>
+          <Chartmonths />
+          <Chartyears />
         </div>
       </div>
     </>

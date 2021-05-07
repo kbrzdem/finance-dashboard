cd0f4283b4837ed1fc282e474458e3d524963ca5
kbrzdem <kbr_ozdm@hotmail.com>
2021-05-07 07:04:54 +0300

    bar grafiği aylar eklendi comp olşturuldu

---
+++ src/components/chartmonths.js
@@ -0,0 +1,19 @@
+const  chartmonths= () => {
+        return(
+                <div className="profile__chartmonths">
+                    <p className="profile__chartmonths profile__chartmonths--text">Mon</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Tue</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">wed</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Thu</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Fri</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Sat</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Sun</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Mon</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Tue</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Wed</p>
+                    <p className="profile__chartmonths profile__chartmonths--text">Thu</p>
+                </div>
+        )
+};
+
+export default chartmonths
